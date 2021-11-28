65a5f24a09ebdcd88fc4c177d7823adf61f00234
Vinicius <tognivini@hotmail.com>
2021-11-28 17:21:31 -0300

    develop/ primeira aplicação de ngIf e errorField

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -7,11 +7,42 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  isRegisterPage: boolean;
+  stringg: string;
+
+  errorRePassowrd: string;
+
+  onFlipPage() {
+    this.isRegisterPage = !this.isRegisterPage;
+  }
+
   onSubmit(f: NgForm) {
+    if (this.isRegisterPage) {
+      const { password, rePassword } = f.value;
+      if (password === rePassword) {
+        console.log('action register');
+        this.errorRePassowrd = '';
+      } else {
+        this.errorRePassowrd = 'senhas não coincidem';
+        // this.errorFields = {
+        //   ...this.errorFields,
+        //   rePassword: 'Senhas não coincidem',
+        // };
+      }
+    } else {
+      console.log('action login');
+    }
     console.log(f.value);
   }
 
-  constructor() {}
+  constructor() {
+    this.isRegisterPage = false;
+    this.stringg = 'alo';
+    this.errorRePassowrd = '';
+    // this.errorFields = {
+    //   rePassword: false,
+    // };
+  }
 
   ngOnInit(): void {}
 }
