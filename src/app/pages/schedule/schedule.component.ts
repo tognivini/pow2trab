7051f157678f227aab5a2c8e849c3520751891f8
Vinicius <tognivini@hotmail.com>
2021-11-29 03:04:46 -0300

    develop/ adição de interface schedule

--- src/app/pages/schedule/schedule.component.ts
+++ src/app/pages/schedule/schedule.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+// import { LoginService } from '../../service/login/login.service';
+import { NgForm } from '@angular/forms';
+import { ScheduleService } from '../../service/schedule/schedule.service';
 
 @Component({
   selector: 'app-schedule',
   templateUrl: './schedule.component.html',
-  styleUrls: ['./schedule.component.css']
+  styleUrls: ['./schedule.component.css'],
 })
 export class ScheduleComponent implements OnInit {
+  avaliableWashMachine: any = [];
 
-  constructor() { }
+  avaliableHours: any = [];
 
-  ngOnInit(): void {
+  avaliableInterval: any = [
+    {
+      id: '001',
+      value: '00',
+    },
+    {
+      id: '002',
+      value: '30',
+    },
+  ];
+
+  onSubmit(f: NgForm) {
+    // console.log(f.value);
   }
 
+  constructor(private scheduleService: ScheduleService) {
+    this.avaliableWashMachine = this.scheduleService.getAvaliableWashMachine();
+    this.avaliableHours = this.scheduleService.getAvaliableHours();
+  }
+
+  ngOnInit(): void {}
 }
