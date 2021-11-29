de97de8f4b974515891e68cb9a1ba59c9ee43578
Vinicius <tognivini@hotmail.com>
2021-11-28 21:41:08 -0300

    develop/ implementação de localstorage service

--- src/app/service/login/login.service.ts
+++ src/app/service/login/login.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { LocalStorageService } from '../localStorage/local-storage.service';
+import { LocalStorageService } from '../localStorage/localStorage.service';
 
 @Injectable({
   providedIn: 'root',
